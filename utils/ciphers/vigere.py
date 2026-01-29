554d21c4d3158177e84ab1da49595dbbcfe2dec6
josuemj <98498772+josuemj@users.noreply.github.com>
2026-01-28 23:52:46 -0600

    Implement Vigenère decryption function with detailed docstring

--- utils/ciphers/vigere.py
+++ utils/ciphers/vigere.py
@@ -29,3 +29,37 @@ def vigere_encrypt(text: str, key: str, alphabet: list) -> str:
         
         result += alphabet[ciphered_char]
     return result
+
+def vigere_decrypt(text: str, key: str, alphabet: list) -> str:
+    """
+    Decrypts the given text using the Vigenère cipher with the provided key and alphabet.
+    Parameters:
+    - text (str): The input text to be encrypted.
+    - key (str): The encryption key.
+    - alphabet (list): The list of characters representing the alphabet to be used.
+    """
+    n = len(text)
+    N = len(alphabet)
+    key_length = len(key)
+    result = ""
+    if key_length == n:
+        pass # One time pad
+    elif key_length < n:
+        # Repeat the key to match the length of the alphabet
+        key = (key * (n // key_length)) + key[:n % key_length]
+    elif key_length > n:
+        # Truncate the key to match the length of the alphabet
+        key = key[:n]
+            
+    for i in range(len(text)):
+        
+        p_i = alphabet.index(text[i])
+        
+        k_i_mid_k = alphabet.index(key[i])
+        
+        decrypted_char = (p_i - k_i_mid_k) % N
+        
+        result += alphabet[decrypted_char]
+    return result
+
+
